2298f7d44bc94820637fee86c3878358d7879f86
Yhago Salim <yhagosalim.bastos@estudante.ufjf.br>
2023-12-04 12:27:41 -0300

    corrige main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,6 @@
-import { getJoao, getMaria, setJoao, setMaria, deJoaoParaMaria, deMariaParaJoao } from "./joaoEMaria.js";
+import { getJoao, getMaria, deJoaoParaMaria} from "./joaoEMaria.js";
 
 console.log('módulo main');
-
-console.log('João tem ', getJoao(),' maçãs!');
-console.log('Maria tem ', getMaria(),' maçãs!');
-
-setJoao(5);
 
 console.log('João tem ', getJoao(),' maçãs!');
 console.log('Maria tem ', getMaria(),' maçãs!');
